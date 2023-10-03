8973ef10c4dde7ec02f09952f3e035efa74c4485
Vadym Kaninskyi <vadim.cpp@gmail.com>
2023-10-03 20:46:53 +0200

    #13 fix room logic

--- src/components/guess-the-card.tsx
+++ src/components/guess-the-card.tsx
@@ -12,19 +12,17 @@ const GuessTheCard: React.FC = () => {
   const leaderUid: string | undefined = room?.leaderUid
   const currentExplainer: User | undefined = leaderUid ? players.find((player: User) => player.uid === leaderUid) : undefined
 
-  if (!currentExplainer) {
-    return null
-  }
-
   return (
     <>
-      <section className="mx-4 text-center">
-        <div className="w-24 h-24 mx-auto rounded-full">
-          <QuestionMarkCircleIcon />
-        </div>
-        <h2 className="text-xl font-semibold mt-4">Guess the word.</h2>
-        <p className="mt-4">{currentExplainer.displayName} is explaining the word now. Guess it!</p>
-      </section>
+      { !!currentExplainer && (
+        <section className="mx-4 text-center">
+          <div className="w-24 h-24 mx-auto rounded-full">
+            <QuestionMarkCircleIcon />
+          </div>
+          <h2 className="text-xl font-semibold mt-4">Guess the word.</h2>
+          <p className="mt-4">{currentExplainer.displayName} is explaining the word now. Guess it!</p>
+        </section>
+      )}
       <ResetGame />
     </>
   )
