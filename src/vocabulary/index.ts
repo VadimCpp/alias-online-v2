72cada0dd8133d303d31821079f220264abfd548
Vadim Cpp <vadim.cpp@gmail.com>
2024-02-01 16:05:01 +0100

    #19 fix type warnings

--- src/vocabulary/index.ts
+++ src/vocabulary/index.ts
@@ -1,6 +1,7 @@
 import Vocabulary from './vocabulary.json';
 import VocabularyTest from './vocabulary-test.json';
+import * as Types from '../types'
 
-const vocabulary = (import.meta.env.MODE === 'development') ? VocabularyTest : Vocabulary;
+const vocabulary: Types.Card[] = (import.meta.env.MODE === 'development') ? VocabularyTest : Vocabulary;
 
 export default vocabulary
